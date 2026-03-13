2ce44e64b48e26ff9f9315223b10731c87bd3cad
favivito <favianeloy@gmail.com>
2026-03-13 11:58:06 +0100

    boton

--- Interfaz/Form1.Designer.cs
+++ Interfaz/Form1.Designer.cs
@@ -28,24 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // button2
             // 
-            this.button1.Location = new System.Drawing.Point(671, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 57);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Botón Nico";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(671, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "buton favian";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,8 +54,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Interfaz/Program.cs
+++ Interfaz/Program.cs
@@ -17,6 +17,10 @@ namespace Interfaz
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+
         }
     }
 }
