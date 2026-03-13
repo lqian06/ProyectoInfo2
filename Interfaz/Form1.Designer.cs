b3bcac6b89976a69c5e23909a5e1c2f6671bbab3
favivito <favianeloy@gmail.com>
2026-03-13 12:00:19 +0100

    prueba test

--- Interfaz/Form1.Designer.cs
+++ Interfaz/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 56);
             this.button2.TabIndex = 1;
-            this.button2.Text = "buton favian";
+            this.button2.Text = "boton favian";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
